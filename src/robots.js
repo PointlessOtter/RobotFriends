3601cdee947bad60d6cc113af266bb2a1d22d970
PointlessOtter <eriks.dombrovskis@gmail.com>
2018-06-13 14:07:03 +0300

    adding files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Benedict Cumberbatch',
+	username: 'writhingpolonium',
+	email: 'skup@gmail.com'
+},
+{
+	id: 2,
+	name: 'Benedryl Cabbagepatch ',
+	username: 'shotgunpascal',
+	email: 'tender@gmail.com'
+},
+{
+	id: 3,
+	name: 'Wimbledon Tennismatch',
+	username: 'praycomponents',
+	email: 'fast@gmail.com'
+},
+{
+	id: 4,
+	name: 'Bendycat Cookiebatch',
+	username: 'beengear',
+	email: 'est@gmail.com'
+},
+{
+	id: 5,
+	name: 'Bandicoot Thundersnatch',
+	username: 'chortlesilt',
+	email: 'underchung@gmail.com'
+},
+{
+	id: 6,
+	name: 'Breadmachine Crackerjack',
+	username: 'wardroombland',
+	email: 'what@gmail.com'
+},
+{
+	id: 7,
+	name: 'Cucumber Bandersnatch',
+	username: 'pencilhelvetica',
+	email: 'lowut@gmail.com'
+},
+{
+	id: 8,
+	name: 'Bendable Cummerbund',
+	username: 'highlyequip',
+	email: 'high@gmail.com'
+},
+{
+	id: 9,
+	name: 'Benevolent Computerglitch',
+	username: 'abrupthumongous',
+	email: 'qwe@gmail.com'
+},
+{
+	id: 10,
+	name: 'Bumblebee Baggageclaim',
+	username: 'cooleaster',
+	email: 'camper@gmail.com'
+},
+]
